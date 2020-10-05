a71440a152cda76f0641d4b79b6dd9655076f166
David Craven <david@craven.ch>
2020-10-05 13:02:47 +0200

    Box `BitswapStore`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 //! Bitswap protocol implementation
+#![deny(missing_docs)]
+#![deny(warnings)]
+
 mod behaviour;
 mod protocol;
 mod query;
 
 pub use crate::behaviour::{Bitswap, BitswapConfig, BitswapEvent, BitswapStore};
-pub use crate::query::BitswapSync;
+pub use crate::query::{BitswapSync, Query, QueryResult, QueryType};
