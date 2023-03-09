8c544d0d2ef6a0919cfd4f7760a870d4d113f874
Akosh Farkash <aakoshh@gmail.com>
2023-03-09 12:57:48 +0000

    Make BitswapRequest and BitswapResponse public.

--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,5 @@ mod query;
 mod stats;
 
 pub use crate::behaviour::{Bitswap, BitswapConfig, BitswapEvent, BitswapStore, Channel};
+pub use crate::protocol::{BitswapRequest, BitswapResponse};
 pub use crate::query::QueryId;
